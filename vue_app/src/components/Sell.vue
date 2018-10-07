089184806ed5ce7f9ca0606fee1e67cd74f1bea8
schekuri1 <schekuri1@bloomberg.net>
2018-10-06 22:04:28 -0700

    add new files

---
+++ vue_app/src/components/Sell.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-container fluid>
+    <v-layout>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+          <!--<v-img-->
+          <!--src="/static/tesla.jpg"-->
+          <!--aspect-ratio="2.75"-->
+          <!--&gt;</v-img>-->
+
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+              <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
+            </div>
+          </v-card-title>
+
+          <v-card-actions>
+            <v-btn flat color="orange">Share</v-btn>
+            <v-btn flat color="orange">Explore</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  import {getSell} from './api'
+
+  export default {
+    data () {
+      return {
+        buyer: []
+      }
+    },
+
+    methods: {
+      refresh (err, data) {
+        this.buyer = data
+        console.log('refresh : ', data, ' err:', err)
+      }
+    },
+
+    beforeRouteEnter (to, from, next) {
+      getSell(to.params).then((data) => {
+        next(vm => vm.refresh(null, data))
+      })
+    }
+  }
+</script>
